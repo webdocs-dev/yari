388f6b6295d944d2324d46d8f29ca0c98014027e
Ruby Iris Juric <ruby@srxl.me>
2023-07-11 22:37:43 +1000

    Create Nix flake with a development shell

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShell = pkgs.mkShell {
+        name = "yari-devshell";
+
+        nativeBuildInputs = with pkgs; [
+          # Nodejs and JS dev tools
+          nodejs
+          yarn
+          nodePackages.typescript-language-server
+          nodePackages.vscode-css-languageserver-bin
+          nodePackages.vscode-html-languageserver-bin
+
+          # Required for building gifsicle and mozjpeg node packages
+          autoconf
+          automake
+          libtool
+          nasm
+          pkg-config
+
+          # Other tools
+          git
+        ];
+      };
+    });
+}
