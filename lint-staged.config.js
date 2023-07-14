967da86e8bfb19e3bafb514030f16e80f4a0f479
Michael[tm] Smith <mike@w3.org>
2023-07-14 19:20:31 +0900

    Make commit hook do markdown linting (#21)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,4 +2,5 @@ export default {
   "!*.{js,jsx,ts,tsx,css,scss}": "prettier --ignore-unknown --write",
   "*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write"],
   "*.{css,scss}": ["stylelint --fix --allow-empty-input", "prettier --write"],
+  "*.md": ["npx markdownlint-cli --fix"],
 };
